afdb4db48f3f4001d99f5c3cf076922da43aef1b
Kalunnenna1 <nnennakalualie.com>
2022-08-15 16:14:43 -0700

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,64 +1,48 @@
 #include "sort.h"
 
 /**
-* exchange - swaps
-* @array: the array
-* @a: the number
-* @b: dj khaled another one
-*
-* Return: none it is vopiderioinooooo
-*/
-void exchange(int *array, int a, int b)
+ * get_max_gap - gets the a largest Knuth Sequence gap for this size
+ * @size: the size of the array
+ *
+ * Return: the gap size
+ */
+size_t get_max_gap(size_t size)
 {
-	int temp;
+	size_t n;
 
-	temp = array[a];
-	array[a] = array[b];
-	array[b] = temp;
+	n = 1;
+	while (n < size)
+		n = n * 3 + 1;
+	return ((n - 1) / 3);
 }
 
 /**
-* less - checks if one number is less than another
-* @a: a number
-* @b: another number
-*
-* Return: pseudo boolean. it is either 1/ true or 0/ false
-*/
-int less(int a, int b)
-{
-	if (a < b)
-		return (1);
-	return (0);
-}
-
-/**
-* shell_sort - shell sorts
-* @array: array
-* @size: the size of array
-*/
-
+ * shell_sort - shell_sort
+ * @array: the integer array to sort
+ * @size: the size of the array
+ *
+ * Return: void
+ */
 void shell_sort(int *array, size_t size)
 {
-	size_t length = size;
-	unsigned int x = 1;
-	unsigned int i, j;
+	size_t gap, i, j;
+	int temp;
 
-	if (!array || size < 2)
+	if (!array || !size)
 		return;
 
-	while (x < length / 3)
-		x = 3 * x + 1;
 
-	while (x >= 1)
+	for (gap = get_max_gap(size); gap; gap = (gap - 1) / 3)
 	{
-		for (i = x; i < length; i++)
+		for (i = gap; i < size; i++)
 		{
-			for (j = i; j >= x && less(array[j], array[j - x]); j -= x)
+			temp = array[i];
+			for (j = i; j > gap - 1 && array[j - gap] > temp; j -= gap)
 			{
-				exchange(array, j, j - x);
+				array[j] = array[j - gap];
 			}
+			array[j] = temp;
 		}
-		x /= 3;
 		print_array(array, size);
 	}
 }
