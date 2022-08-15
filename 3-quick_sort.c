2a01991d08428be2b2e45eb5c893afc514efd8ae
Kalunnenna1 <nnennakalualie.com>
2022-08-15 14:05:44 -0700

    Quick Sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,81 +1,76 @@
 #include "sort.h"
 
 /**
- * quick_sort - partitions sorts by half
- * @array: the array
- * @size: size of length
+ * swap - swaps 2 int values
+ * @array: the integer array to sort
+ * @size: the size of the array
+ * @a: address of first value
+ * @b: address of second value
+ *
  * Return: void
  */
-void quick_sort(int *array, size_t size)
+void swap(int *array, size_t size, int *a, int *b)
 {
-	if (size < 2)
-		return;
-
-	_qsort(array, 0, size - 1, size);
-}
-
-/**
- * _qsort - recursively calls itself to move left and right
- * @array: the array
- * @left: left pointer
- * @right: right pointer
- * @size: size of length
- * Return: void
- */
-void _qsort(int *array, int left, int right, size_t size)
-{
-	int index = partition(array, left, right, size);
-
-	if (left >= right)
-		return;
-
-	_qsort(array, left, index - 1, size);
-	_qsort(array, index + 1, right, size);
-}
-
-/**
- * partition - partition sorts by pivot point
- * @array: the array
- * @left: left pointer
- * @right: right pointer
- * @size: size of array length
- * Return: void
- */
-size_t partition(int *array, int left, int right, size_t size)
-{
-	int pivot = array[right];
-	int i = left, j = 0;
-
-	for (j = left; j < right; j++)
+	if (*a != *b)
 	{
+		*a = *a + *b;
+		*b = *a - *b;
+		*a = *a - *b;
+		print_array((const int *)array, size);
+	}
+}
+
+/**
+ * lomuto_partition - partitions the array
+ * @array: the integer array to sort
+ * @size: the size of the array
+ * @lo: the low index of the sort range
+ * @hi: the high index of the sort range
+ *
+ * Return: void
+ */
+size_t lomuto_partition(int *array, size_t size, ssize_t lo, ssize_t hi)
+{
+	int i, j, pivot = array[hi];
+
+	for (i = j = lo; j < hi; j++)
 		if (array[j] < pivot)
-		{
-			if (i != j && array[i] != array[j])
-			{
-				swap(array, i, j);
-				print_array(array, size);
-			}
-			i++;
-		}
-	}
-	if (i != right && array[i] != array[j])
-	{
-		swap(array, i, right);
-		print_array(array, size);
-	}
+			swap(array, size, &array[j], &array[i++]);
+	swap(array, size, &array[i], &array[hi]);
+
 	return (i);
 }
 
 /**
- * swap - swaps two given indexes
- * @array: the array
- * @left: left pointer
- * @right: right pointer
+ * quicksort - quicksorts via Lomuto partitioning scheme
+ * @array: the integer array to sort
+ * @size: the size of the array
+ * @lo: the low index of the sort range
+ * @hi: the high index of the sort range
+ *
  * Return: void
  */
-void swap(int *array, int left, int right)
+void quicksort(int *array, size_t size, ssize_t lo, ssize_t hi)
 {
-	array[left] += array[right];
-	array[right] = array[left] - array[right];
-	array[left] -= array[right];
+	if (lo < hi)
+	{
+		size_t p = lomuto_partition(array, size, lo, hi);
+
+		quicksort(array, size, lo, p - 1);
+		quicksort(array, size, p + 1, hi);
+	}
+}
+
+/**
+ * quick_sort - calls quicksort
+ * @array: the integer array to sort
+ * @size: the size of the array
+ *
+ * Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || !size)
+		return;
+	quicksort(array, size, 0, size - 1);
 }
